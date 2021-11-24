20584006697f3f3bc0a7b78e8f44d0663e7d0092
@chavidev <javiercastaneda303@gmail.com>
2021-11-24 17:43:09 +0000

    _inicio router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 import './App.css';
+import LoguinAdmin from "./views/LoguinAdmin";
+import LoguinCliente from "./views/LoguinCliente";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+    <h1>Welcome to React Router!</h1>
+    <Routes>
+      <Route path="/loguinadmin" element={<LoguinAdmin />} />
+      <Route path="/loguin" element={<LoguinCliente />} />
+    </Routes>
     </div>
   );
 }
---
+++ src/views/LoguinAdmin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoguinAdmin = () => {
+  return (
+    <div>
+      Loguin Admin
+    </div>
+  )
+}
+
+export default LoguinAdmin
+//rca
