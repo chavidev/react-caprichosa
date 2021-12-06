0d66d8d1d2716906570bbd0762a541f4120ed908
@chavidev <javiercastaneda303@gmail.com>
2021-12-06 18:51:56 +0000

    _

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { SpinnerStateProvider } from './providers/SpinnerProvider'
+
 ReactDOM.render(
   <BrowserRouter>
     <SpinnerStateProvider>
@@ -18,4 +19,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals() //&&¿Ésto que es?
